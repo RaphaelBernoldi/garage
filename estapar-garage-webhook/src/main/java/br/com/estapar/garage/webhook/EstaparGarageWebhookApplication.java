0135cfb311f439641e89c19370678e42f6c06b06
Raphael Bernoldi <raphaelbernoldi.triad@grupoccr.com>
2025-05-18 17:01:18 -0300

    creation base project

---
+++ estapar-garage-webhook/src/main/java/br/com/estapar/garage/webhook/EstaparGarageWebhookApplication.java
@@ -0,0 +1,13 @@
+package br.com.estapar.garage.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstaparGarageWebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstaparGarageWebhookApplication.class, args);
+	}
+
+}
---
+++ estapar-garage-webhook/src/test/java/br/com/estapar/garage/webhook/EstaparGarageWebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estapar.garage.webhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstaparGarageWebhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
