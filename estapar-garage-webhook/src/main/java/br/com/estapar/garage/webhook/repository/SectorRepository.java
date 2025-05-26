d6ba68765e019bb4c7b0554fc3cf59682ece2fee
Raphael Bernoldi <raphaelbernoldi.triad@grupoccr.com>
2025-05-26 18:31:14 -0300

    Implementacao de endpoint busca faturamento

--- estapar-garage-webhook/src/main/java/br/com/estapar/garage/webhook/repository/SectorRepository.java
+++ estapar-garage-webhook/src/main/java/br/com/estapar/garage/webhook/repository/SectorRepository.java
@@ -12,4 +12,5 @@ import java.util.UUID;
 public interface SectorRepository extends JpaRepository<SectorEntity, UUID> {
 
     List<SectorEntity>findByDateOperation(LocalDate date);
+    SectorEntity findByDateOperationAndName(LocalDate date, String name);
 }
--- estapar-garage-webhook/src/main/java/br/com/estapar/garage/webhook/rest/RevenueController.java
+++ estapar-garage-webhook/src/main/java/br/com/estapar/garage/webhook/rest/RevenueController.java
@@ -1,13 +1,28 @@
 package br.com.estapar.garage.webhook.rest;
 
+import br.com.estapar.garage.webhook.rest.response.RevenueResponse;
+import br.com.estapar.garage.webhook.service.RevenueService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/revenue")
 @Slf4j
 public class RevenueController {
+
+    private final RevenueService revenueService;
+
+    @GetMapping("/date/{date}/sector/{sector}")
+    public ResponseEntity<RevenueResponse>findRevenueByDateAndSector(@PathVariable("date") LocalDate date, @PathVariable("sector") String sector){
+        log.info("Receiveing request /revenue/date/{}/sector/{}", date, sector);
+        return ResponseEntity.ok(revenueService.findRevenueByDateAndSector(date, sector));
+    }
 }
